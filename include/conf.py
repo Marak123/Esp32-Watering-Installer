679a42bd219084177f2137df66dd9da8ea7b5143
Marak123 <jurek69-00@wp.pl>
2022-05-09 22:28:40 +0200

    Update config py file

--- include/conf.py
+++ include/conf.py
@@ -2,8 +2,8 @@
 ICON_APP_PATH = ".\\src\\logo.ico"
 COMMAND_LOG_FILE = ".\\temp\\log"
 
-GITHUB_BASE_URL = "https://raw.githubusercontent.com/Marak123/test/main/"
-GITHUB_VERSION_URL = "https://raw.githubusercontent.com/Marak123/test/main/version.json"
+GITHUB_BASE_URL = "https://raw.githubusercontent.com/Marak123/Esp32-Watering/main/version/"
+GITHUB_VERSION_URL = "https://raw.githubusercontent.com/Marak123/Esp32-Watering/main/version/version.json"
 
 FRIMWARE_FILE_NAME = "firmware.bin"
 SPIFFS_FILE_NAME = "spiffs.bin"
